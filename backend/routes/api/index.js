4d2fd4fa9e895a0dcb8d650a2a6fe2eb45ae0889
matt-fong <matthewfong12@gmail.com>
2022-08-02 17:35:24 -0400

    creted spot router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
+const spotsRouter = require('./spots.js')
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
@@ -10,8 +11,9 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
+router.use('/spots', spotsRouter);
+
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
