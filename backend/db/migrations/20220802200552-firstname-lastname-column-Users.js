a0bd7239db782b07770499dab438a6c07036649a
matt-fong <matthewfong12@gmail.com>
2022-08-02 16:08:02 -0400

    added firstname lastname column to users

---
+++ backend/db/migrations/20220802200552-firstname-lastname-column-Users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn(
+      'Users',
+      'firstName',
+      {
+        type: Sequelize.STRING
+      },
+    );
+    await queryInterface.addColumn(
+      'Users',
+      'lastName',
+      {
+        type: Sequelize.STRING
+      },
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'firstName');
+    await queryInterface.removeColumn('Users', 'lastName');
+  }
+};
