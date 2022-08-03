d7cbb7cd107f742e6bab40ce90846e8666ab9482
matt-fong <matthewfong12@gmail.com>
2022-08-03 19:17:02 -0400

    add image to review based on reviewer id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,4 +7,49 @@ const { Op } = require("sequelize");
 
 const router = express.Router();
 
+// Add an Image to a Review based on the Review's id
+router.post('/:reviewId/images', requireAuth, restoreUser, async (req, res, next) => {
+  const reviewId = req.params.reviewId
+  const { url, previewImage } = req.body
+  let review = await Review.findByPk(reviewId)
+
+  if (!review) {
+    res.json({
+      message: "Review couldn't be found",
+      statusCode: 404
+    })
+  }
+
+  let image = await Image.create({
+    url: url,
+    previewImage: previewImage,
+    reviewId: reviewId,
+    userId: req.user.id
+  })
+
+  let count = await Image.findAll({
+    where: {
+      [Op.and]:[
+        { reviewId: reviewId }, { previewImage: previewImage }
+      ]
+    }
+  })
+
+  if(count.length >= 10) {
+    res.json({
+      message: "Maximum number of images for this resource was reached",
+      statusCode: 403
+    })
+  }
+
+  let response = {
+    id: image.id,
+    imageableId: image.reviewId,
+    url: image.url
+  }
+
+  res.json(response)
+
+})
+
 module.exports = router;
