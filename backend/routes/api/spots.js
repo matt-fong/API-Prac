95900caaa02e1ab4f291dbc95a61c7fa31dd4459
matt-fong <matthewfong12@gmail.com>
2022-08-03 13:07:23 -0400

    edit and delete spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
 
 const { Booking, Image, Review, Spot, User, sequelize } = require("../../db/models");
 const { Op } = require("sequelize")
@@ -12,7 +12,7 @@ router.get('/', async (req, res, next) => {
   const spots = await Spot.findAll({
     include: [
       { model: Review, attributes: [] },
-      { model: Image, attributes: [], where: {previewImage:true} }
+      { model: Image, attributes: [], where: { previewImage: true } }
     ],
     attributes: {
       include: [
@@ -23,19 +23,22 @@ router.get('/', async (req, res, next) => {
     group: ['Spot.id'],
   })
 
-  res.json({ Spot: spots })
+  res.json({ Spots: spots })
 })
 
-// Get Spots for Current User
-router.get('/current', requireAuth, async (req, res)=>{
-  const currentuserspots = await Spot.findAll({
-    where:{ ownerId: req.user.id }
+// Get all Spots owned by the Current User
+router.get('/current', restoreUser, requireAuth, async (req, res)=>{
+  const spots = await Spot.findAll({
+    include: [{ model: Review, attributes: [] }],
+    attributes: { include: [[ sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating' ]]},
+    group: ['Spot.id'],
+    where: { ownerId: req.user.id },
   });
 
-  res.json(currentuserspots);
+  res.json({ Spots: spots })
 })
 
-// Get Details of a Spot by Id
+// Get details of a Spot from an id
 router.get('/:spotId', async (req, res) => {
   const spotId = req.params.spotId
   const spot = await Spot.findOne({
@@ -71,6 +74,81 @@ router.post('/', requireAuth, async (req, res, next) => {
   })
 
   res.json(spot)
+})
+
+// Add an Image to a Spot based on the Spot's id
+router.post('/:spotId/images', requireAuth, restoreUser, async (req, res, next) => {
+  const spotId = req.params.spotId
+  const { url } = req.body
+  const spot = await Spot.findByPk(spotId)
+
+  if (!spot) {
+    res.json({
+      message:"Spot couldn't be found",
+      statusCode: 404
+    })
+  }
+
+  const image = await Image.create({
+    url,
+    spotId: spot.dataValues.id,
+    userId: req.user.id
+  })
+
+  const response = {
+    id: image.id,
+    imageableId: image.spotId,
+    url: image.url
+  }
+
+  res.json(response)
+
+})
+
+// Edit a Spot
+router.put('/:spotId', requireAuth, async (req, res, next) => {
+  const spotId = req.params.spotId
+  const { address, city, state, country, lat, lng, name, description, price } = req.body
+  const spot = await Spot.findByPk(spotId)
+
+  if (!spot) {
+    res.json({
+      message:"Spot couldn't be found",
+      statusCode: 404
+    })
+  }
+
+  spot.address = address,
+  spot.city = city,
+  spot.state = state,
+  spot.country = country,
+  spot.lat = lat,
+  spot.lng = lng,
+  spot.name = name,
+  spot.description = description,
+  spot.price = price,
+
+  await spot.save()
+  res.json(spot)
+})
+
+// Delete a Spot
+router.delete('/:spotId', requireAuth, async (req, res, next) => {
+  const spotId = req.params.spotId
+  const spot = await Spot.findByPk(spotId)
+
+  if (!spot) {
+    res.json({
+      message: "Spot couldn't be found",
+      statusCode: 404
+    })
+  }
+
+  spot.destroy()
+  res.json({
+    message: "Successfully deleted",
+    statusCode: 200
+  })
 })
 
 module.exports = router;
