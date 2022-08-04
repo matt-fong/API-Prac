6390251f48061f6f1165a78add9e7cb0d447fb24
matt-fong <matthewfong12@gmail.com>
2022-08-04 12:29:30 -0400

    added bookings router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,6 +7,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
+const bookingsRouter = require('./bookings.js')
 
 router.use(restoreUser);
 
@@ -14,11 +15,11 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-
 
 // GET /api/set-token-cookie
 router.get('/set-token-cookie', async (_req, res) => {
