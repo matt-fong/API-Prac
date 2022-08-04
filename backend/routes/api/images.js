f66a99042f246df8b6cd14125746390efa160845
matt-fong <matthewfong12@gmail.com>
2022-08-04 17:10:48 -0400

    added images router

---
+++ backend/routes/api/images.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+
+const { Booking, Image, Review, Spot, User, sequelize } = require("../../db/models");
+const { Op } = require("sequelize");
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,6 +8,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
 const reviewsRouter = require('./reviews.js')
 const bookingsRouter = require('./bookings.js')
+const imagesRouter = require('./images.js')
 
 router.use(restoreUser);
 
@@ -16,6 +17,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/images', imagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
