db923807ecbee213685c247c162fd1e7310c263b
matt-fong <matthewfong12@gmail.com>
2022-08-04 17:14:59 -0400

    delete an image

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -7,6 +7,26 @@ const { Op } = require("sequelize");
 
 const router = express.Router();
 
+// Delete an Image
+router.delete('/:imageId', requireAuth, restoreUser, async (req, res, next) => {
+  const imageId = req.params.imageId
+  const image = await Image.findByPk(imageId)
 
+  if (!image) {
+    res.json({
+      message: "Image couldn't be found",
+      statusCode: 404
+    })
+  }
+
+  if (image.userId === req.user.id) {
+    image.destroy()
+    res.json({
+      message: "Successfully deleted",
+      statusCode: 200
+    })
+  }
+
+})
 
 module.exports = router;
