ca5dc5cbff5deabdc800042ba5f16906a411abf6
matt-fong <matthewfong12@gmail.com>
2022-08-04 14:12:40 -0400

    get all bookings for current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,6 +7,49 @@ const { Op } = require("sequelize");
 
 const router = express.Router();
 
+// Get all of the Current User's Bookings
+router.get('/current', requireAuth, async (req, res, next) => {
+  const bookings = await Booking.findAll({
+    where: { userId: req.user.id }
+  });
 
+  let bookingsArr = []
+
+  for (let booking of bookings) {
+    let spot = await Spot.findOne({
+      where: { id: booking.spotId },
+        raw: true,
+        attributes: ["id", "ownerId", "address", "city", "state", "country", "lat", "lng", "name", "price"]
+    })
+
+    let image = await Image.findOne({
+      where:{
+        spotId: booking.spotId,
+        previewImage: true
+      }
+    })
+
+    if (image) {
+      spot.previewImage = image.url
+    } else {
+      spot.previewImage = null
+    }
+
+    let response = {
+      id: booking.id,
+      spotId: booking.spotId,
+      Spot: spot,
+      userId: booking.userId,
+      startDate: booking.startDate,
+      endDate: booking.endDate,
+      createdAt: booking.createdAt,
+      updatedAt: booking.updatedAt
+    }
+
+    bookingsArr.push(response)
+  }
+
+  res.json({ Bookings: bookingsArr })
+})
 
 module.exports = router;
