f3bd86e2a4cd4e78b0927258b34c386b5097690d
matt-fong <matthewfong12@gmail.com>
2022-08-19 16:33:12 -0400

    Sign Up

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -55,4 +55,21 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password, firstName, lastName } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      firstName,
+      lastName
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data));
+  return response;
+};
+
 export default sessionReducer;
