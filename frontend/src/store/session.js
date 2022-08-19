b00a86881f776ecb98b6894dcc1238543512ed4a
matt-fong <matthewfong12@gmail.com>
2022-08-19 19:18:33 -0400

    Updated Session

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -51,7 +51,6 @@ const sessionReducer = (state = initialState, action) => {
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
-  console.log('This is data', data)
   dispatch(setUser(data));
   return response;
 };
