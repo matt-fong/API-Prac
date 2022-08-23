131d649fbbd7206603f59c6dee00db1b9c99b7f0
matt-fong <matthewfong12@gmail.com>
2022-08-23 01:28:11 -0400

    CSS for Home Page/Spot Card

--- frontend/src/components/SpotCard/index.js
+++ frontend/src/components/SpotCard/index.js
@@ -6,7 +6,18 @@ const SpotCard = ({ spot }) => {
     <Link className='spotCardContainer' to={`/spots/${spot.id}`}>
       <div className='spotCardContainer'>
         <img className='spotImage' src={spot.previewImage} />
-        <div className='spotDescription'>{spot.name}</div>
+        <div className='spotDescription'>
+          <div className='spotDescriptionLeft'>
+            <div className='spotLocation'>{spot.city}, {spot.state}</div>
+            <div className='spotPrice'>{`$${spot.price} night`}</div>
+          </div>
+          <div className='spotDescriptionRight'>
+            <div className='spotRating'>
+              <i class="fa-solid fa-star"></i>
+              <div className='spotRatingNumber'>{spot.avgRating}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </Link>
   )
