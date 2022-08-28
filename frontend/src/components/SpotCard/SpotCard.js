437471617c606e638851bbd5c6f2c5ed00e6f0c1
matt-fong <matthewfong12@gmail.com>
2022-08-28 15:16:54 -0400

    Adjust Spot Cards to Unbold Night

--- frontend/src/components/SpotCard/SpotCard.js
+++ frontend/src/components/SpotCard/SpotCard.js
@@ -9,7 +9,11 @@ const SpotCard = ({ spot }) => {
         <div className='spotDescription'>
           <div className='spotDescriptionLeft'>
             <div className='spotLocation'>{spot.city}, {spot.state}</div>
-            <div className='spotPrice'>{`$${spot.price} night`}</div>
+            {/* <div className='spotPrice'>{`$${spot.price} night`}</div> */}
+            <div className='spotPrice'>
+              <div className='spotPriceAmount'>{`$${spot.price}`}</div>
+              <div className='spotNight'>night</div>
+            </div>
           </div>
           <div className='spotDescriptionRight'>
             <div className='spotStar'>
