1f79b5c884d92843004da1f64f009c72f0a7f389
matt-fong <matthewfong12@gmail.com>
2022-09-05 19:38:38 -0400

    Fixing Inconsistent Edit Spot

--- frontend/src/components/EditSpot/EditSpot.js
+++ frontend/src/components/EditSpot/EditSpot.js
@@ -47,8 +47,8 @@ function EditSpotForm({ onX }) {
     if (country) data.country = country;
 
     setErrors([]);
-    dispatch(editSpot(data, spot.id))
-    dispatch(getAllSpots())
+    dispatch(editSpot(data, spot.id)).then(() => dispatch(getAllSpots()))
+    // dispatch(getAllSpots())
     // .catch(async (res) => {
     //   const data = await res.json();
     //   if (data && data.errors) setErrors(data.errors);
