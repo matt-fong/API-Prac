69112788c677e6ca14e5ce0480bdba745e59e92a
matt-fong <matthewfong12@gmail.com>
2022-09-08 16:57:32 -0400

    Created Images Reducer

---
+++ frontend/src/store/images.js
@@ -0,0 +1,60 @@
+import { csrfFetch } from "./csrf";
+
+const CREATE = "images/CREATE";
+const DELETE = "images/DELETE";
+
+
+export const createImage = (review) => {
+  return {
+    type: CREATE,
+    review,
+  };
+};
+
+export const createImageBySpotId = (spotId, imageData) => async (dispatch) => {
+  const reqData = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(imageData),
+  };
+  const res = await csrfFetch(`/api/spots/${spotId}/images`, reqData);
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(createImage(data));
+  }
+  return res;
+};
+
+export const deleteImage = (imageId) => {
+  return {
+    type: DELETE,
+    imageId,
+  };
+};
+
+export const deleteImageById = (imageId) => async (dispatch) => {
+  const reqData = {
+    method: "DELETE",
+  };
+  const res = await csrfFetch(`/api/images/${imageId}`, reqData);
+  if (res.ok) {
+    dispatch(deleteImage(imageId));
+  }
+  return res;
+};
+
+export default function imagesReducer(state = {}, action) {
+  let newState;
+  switch (action.type) {
+    case CREATE:
+      newState = { ...state };
+      newState[action.review.id] = action.review;
+      return newState;
+    case DELETE:
+      newState = { ...state };
+      delete newState[action.reviewId];
+      return newState;
+    default:
+      return state;
+  }
+}
