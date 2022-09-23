589c3ed55e77beacba1aaa52a484008a392867b7
matt-fong <matthewfong12@gmail.com>
2022-09-22 22:45:29 -0400

    Added CSS/Conditional For User Reviews

--- frontend/src/components/UserReviews/UserReviews.js
+++ frontend/src/components/UserReviews/UserReviews.js
@@ -8,6 +8,7 @@ import ReviewCard from "../ReviewCard/ReviewCard";
 import './UserReviews.css'
 import { getAllUsers } from "../../store/users";
 import EditReviewModal from "../EditReview/EditReviewModal";
+import CreateSpotModalTwo from "../CreateSpot/CreateSpotModalTwo";
 
 const UserReviews = () => {
   const reviews = useSelector(state => Object.values(state.reviews));
@@ -31,27 +32,54 @@ const UserReviews = () => {
     dispatch(deleteReviewById(reviewId, spotId));
   };
 
+  let userNoReviews;
+
+  if (Object.keys(reviews).length === 0) {
+    userNoReviews = (
+      <div className="UserReviewsLayoutTwo">
+        <div className="user-booking-notrip-container">
+          <div className="user-booking-notrip-inner-container">
+            <div className="user-booking-notrip-left">
+              <img className="user-booking-notrip-hand" src='https://images.emojiterra.com/google/android-10/512px/1f44b.png' alt=''></img>
+              <div className="user-booking-notrip-header">No reviews...yet!</div>
+              <div className="user-booking-notrip-undertext">Time to start sharing your experience with other Airdnd members</div>
+              {/* <button className="user-booking-notrip-search-spots"><CreateSpotModal /></button> */}
+              {/* <CreateSpotModalTwo /> */}
+              {/* <div><CreateSpotModal /></div> */}
+              <button className="user-booking-notrip-search" onClick={() => history.push('/')}>Start reviewing</button>
+            </div>
+            <img className="user-booking-notrip-right" src='https://a0.muscache.com/im/pictures/d727f355-3f10-44b5-9750-d1efca2438fc.jpg?im_w=720' alt=''></img>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    userNoReviews = (
+      <div className="UserReviewsLayout">
+        {reviews.map((review, i) => (
+          <div className="userReviewCardsContainer" key={i}>
+
+            <div>
+              <ReviewCard key={review?.id} review={review}/>
+            </div>
+
+            <div className="userSpotsButtons">
+              <div><EditReviewModal reviewId={review.id}/></div>
+              <button className="userSpotsDeleteButton" onClick={() => handleDelete(review.id, review.spotId)}>Delete</button>
+            </div>
+
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <div className="userReviewContainer">
       <div className="userReviewInnerContainer">
         <div className="userReviewHeader">User Reviews</div>
         <div className="userReviewCardContainer">
-        <div className="spotLayout">
-          {reviews.map((review, i) => (
-            <div key={i}>
-
-              <div>
-                <ReviewCard key={review?.id} review={review}/>
-              </div>
-
-              <div className="userSpotsButtons">
-                <div><EditReviewModal reviewId={review.id}/></div>
-                <button className="userSpotsDeleteButton" onClick={() => handleDelete(review.id, review.spotId)}>Delete</button>
-              </div>
-
-            </div>
-          ))}
-        </div>
+          {userNoReviews}
         </div>
       </div>
     </div>
