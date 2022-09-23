675163479ed8140439dd339e74e6104c52dd0dcc
matt-fong <matthewfong12@gmail.com>
2022-09-22 21:50:24 -0400

    Fixed CSS On User Spots

--- frontend/src/components/UserSpots/UserSpots.js
+++ frontend/src/components/UserSpots/UserSpots.js
@@ -3,8 +3,7 @@ import { useSelector } from "react-redux";
 import SpotCard from "../SpotCard/SpotCard";
 import './UserSpots.css'
 import '../HomePage/HomePage.css'
-import { NavLink, useHistory } from "react-router-dom";
-import CreateSpotModal from '../CreateSpot/CreateSpotModal';
+import { useHistory } from "react-router-dom";
 import CreateSpotModalTwo from "../CreateSpot/CreateSpotModalTwo";
 
 const UserSpots = () => {
@@ -23,7 +22,7 @@ const UserSpots = () => {
           <div className="user-booking-notrip-left">
             <img className="user-booking-notrip-hand" src='https://images.emojiterra.com/google/android-10/512px/1f44b.png' alt=''></img>
             <div className="user-booking-notrip-header">No spots listed...yet!</div>
-            <div className="user-booking-notrip-undertext">Time to dust off your spot and start sharing your home</div>
+            <div className="user-booking-notrip-undertext">Time to start sharing your home with other Airdnd members</div>
             {/* <button className="user-booking-notrip-search-spots"><CreateSpotModal /></button> */}
             <CreateSpotModalTwo />
             {/* <div><CreateSpotModal /></div> */}
@@ -36,19 +35,21 @@ const UserSpots = () => {
 
   return (
     <div className="userSpotsContainer">
-      <div className="userSpotsHeader">My Listings</div>
-      <div className="spotsContainer">
-        <div className="spotLayout">
-          {userSpots.map((spot, i) => (
-            <div key={i}>
+      <div className="userSpotsInnerContainer">
+        <div className="userSpotsHeader">My Listings</div>
+        <div className="spotsContainer">
+          <div className="UserSpotsLayout">
+            {userSpots.map((spot, i) => (
+              <div key={i}>
 
-              <div>
-                <SpotCard key={spot?.id} spot={spot}/>
+                <div>
+                  <SpotCard key={spot?.id} spot={spot}/>
+                </div>
+
               </div>
-
-            </div>
-          ))}
-          {userNoSpots}
+            ))}
+            {userNoSpots}
+          </div>
         </div>
       </div>
     </div>
