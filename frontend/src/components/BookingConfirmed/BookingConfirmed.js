8ac71cc0d4175780fc84e0a80f217eb64782d615
matt-fong <matthewfong12@gmail.com>
2022-11-30 00:34:28 -0500

    Fixed CSS For Booking Confirmed/Added Map API

--- frontend/src/components/BookingConfirmed/BookingConfirmed.js
+++ frontend/src/components/BookingConfirmed/BookingConfirmed.js
@@ -62,73 +62,76 @@ const BookingConfirmed = () => {
   }
 
   return (
-    <div className="booking-confirmed-container">
-      <div className="booking-confirmed-inner-container">
+    <div className="booking-confirmed-outer-container">
+      <div className="booking-confirmed-container">
+        <div className="booking-confirmed-inner-container">
 
-        <div className="booking-confirmed-image-container">
-          <img className="booking-confirmed-image" src={spot?.previewImage} alt=''></img>
-        </div>
-
-        <div className="booking-confirmed-information-container">
-
-          <div className="booking-confirmed-checkin-checkout-container">
-            <div className="booking-confirmed-check-container">
-
-              <div className="booking-confirmed-checkin-container">
-                <div className="booking-confirmed-checkin">Check-in</div>
-                <div className="booking-confirmed-checkin-date">{`${weekday(startInt)}, ${currentBooked?.startDate}`}</div>
-                <div className="booking-confirmed-checkin-time">4:00PM</div>
-              </div>
-
-              <div className="booking-confirmed-checkout-container">
-                <div className="booking-confirmed-checkout">Checkout</div>
-                <div className="booking-confirmed-checkout-date">{`${weekday(endInt)}, ${currentBooked?.endDate}`}</div>
-                <div className="booking-confirmed-checkout-time">11:00AM</div>
-              </div>
-
-            </div>
+          <div className="booking-confirmed-image-container">
+            <img className="booking-confirmed-image" src={spot?.previewImage} alt=''></img>
           </div>
 
-          <div className="booking-confirmed-details-container">
+          <div className="booking-confirmed-information-container">
 
-            <div className="booking-confirmed-reservation-details-container">
-              <div className="booking-confirmed-reservation-details">Reservation Details</div>
-              <div className="booking-confirmed-who-coming">Who's coming</div>
-              <div className="booking-confirmed-guests">2 guests</div>
+            <div className="booking-confirmed-checkin-checkout-container">
+              <div className="booking-confirmed-check-container">
+
+                <div className="booking-confirmed-checkin-container">
+                  <div className="booking-confirmed-checkin">Check-in</div>
+                  <div className="booking-confirmed-checkin-date">{`${weekday(startInt)}, ${currentBooked?.startDate}`}</div>
+                  <div className="booking-confirmed-checkin-time">4:00PM</div>
+                </div>
+
+                <div className="booking-confirmed-checkout-container">
+                  <div className="booking-confirmed-checkout">Checkout</div>
+                  <div className="booking-confirmed-checkout-date">{`${weekday(endInt)}, ${currentBooked?.endDate}`}</div>
+                  <div className="booking-confirmed-checkout-time">11:00AM</div>
+                </div>
+
+              </div>
             </div>
 
-            <div className="booking-confirmed-divider"></div>
+            <div className="booking-confirmed-details-container">
 
-            <div className="booking-confirmed-getting-there-container">
-              <div className="booking-confirmed-getting-there">Getting there</div>
-              <div className="booking-confirmed-address">Address</div>
-              <div className="booking-confirmed-address-info-top">{spot?.address}</div>
-              <div className="booking-confirmed-address-info-bottom">{spot?.city}, {spot?.state}</div>
-            </div>
+              <div className="booking-confirmed-reservation-details-container">
+                <div className="booking-confirmed-reservation-details">Reservation Details</div>
+                <div className="booking-confirmed-who-coming">Who's coming</div>
+                <div className="booking-confirmed-guests">2 guests</div>
+              </div>
 
-            <div className="booking-confirmed-divider"></div>
+              <div className="booking-confirmed-divider"></div>
 
-            <div className="booking-confirmed-staying-container">
-              <div className="booking-confirmed-where-staying">Where you're staying</div>
-              <div className="booking-confirmed-house-rules">House Rules</div>
-              <div className="booking-confirmed-house-rules-info">No smoking, no drugs, no parties allowed. Do not disturb your neighbors. Do not break anything. If there are any issues, please reach out to me directly.</div>
-            </div>
+              <div className="booking-confirmed-getting-there-container">
+                <div className="booking-confirmed-getting-there">Getting there</div>
+                <div className="booking-confirmed-address">Address</div>
+                <div className="booking-confirmed-address-info-top">{spot?.address}</div>
+                <div className="booking-confirmed-address-info-bottom">{spot?.city}, {spot?.state}</div>
+              </div>
 
-            <div className="booking-confirmed-divider"></div>
+              <div className="booking-confirmed-divider"></div>
 
-            <div className="booking-confirmed-hosted-by-container">
-              <div className="booking-confirmed-hosted-by">Hosted by {spotOwner?.firstName}</div>
-              <div className="booking-confirmed-host-about">About your host</div>
-              <div className="booking-confirmed-host-about-info">I love traveling a lot on Airdnd. Being a traveler that has used Airdnd has made me more interested in becoming a host. I hope you enjoy your Airdnd stay!</div>
-              <div className="booking-confirmed-host-profile">Show Profile</div>
-            </div>
+              <div className="booking-confirmed-staying-container">
+                <div className="booking-confirmed-where-staying">Where you're staying</div>
+                <div className="booking-confirmed-house-rules">House Rules</div>
+                <div className="booking-confirmed-house-rules-info">No smoking, no drugs, no parties allowed. Do not disturb your neighbors. Do not break anything. If there are any issues, please reach out to me directly.</div>
+              </div>
 
-            <div className="booking-confirmed-divider"></div>
+              <div className="booking-confirmed-divider"></div>
 
-            <div className="booking-confirmed-payment-info-container">
-            <div className="booking-confirmed-payment-info">Payment info </div>
-              <div className="booking-confirmed-payment-details">Payment details </div>
-              <div className="booking-confirmed-total-cost">{`Total cost: $${dateDiffInt * spot?.price} USD`}</div>
+              <div className="booking-confirmed-hosted-by-container">
+                <div className="booking-confirmed-hosted-by">Hosted by {spotOwner?.firstName}</div>
+                <div className="booking-confirmed-host-about">About your host</div>
+                <div className="booking-confirmed-host-about-info">I love traveling a lot on Airdnd. Being a traveler that has used Airdnd has made me more interested in becoming a host. I hope you enjoy your Airdnd stay!</div>
+                <div className="booking-confirmed-host-profile">Show Profile</div>
+              </div>
+
+              <div className="booking-confirmed-divider"></div>
+
+              <div className="booking-confirmed-payment-info-container">
+              <div className="booking-confirmed-payment-info">Payment info </div>
+                <div className="booking-confirmed-payment-details">Payment details </div>
+                <div className="booking-confirmed-total-cost">{`Total cost: $${dateDiffInt * spot?.price} USD`}</div>
+              </div>
+
             </div>
 
           </div>
@@ -136,7 +139,6 @@ const BookingConfirmed = () => {
         </div>
 
       </div>
-
       <div className="booking-confirmed-map-container">
         <MapContainer />
       </div>
