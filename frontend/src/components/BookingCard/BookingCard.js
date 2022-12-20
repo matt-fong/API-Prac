6bbb44447f18db246751fedeb15e49a38c3b00a4
matt-fong <matthewfong12@gmail.com>
2022-12-20 16:52:44 -0500

    Adjusted Booking Card Dates

--- frontend/src/components/BookingCard/BookingCard.js
+++ frontend/src/components/BookingCard/BookingCard.js
@@ -5,27 +5,45 @@ const BookingCard = ({ booking }) => {
 
   console.log('this is booking', booking)
 
+  const startInt = new Date(booking?.startDate).getDay()
+
+
+  const endInt = new Date(booking?.endDate).getDay()
+
+
+  const weekday = (day) => {
+    if (day === 6) return 'Sun'
+    if (day === 0) return 'Mon'
+    if (day === 1) return 'Tue'
+    if (day === 2) return 'Wed'
+    if (day === 3) return 'Thu'
+    if (day === 4) return 'Fri'
+    if (day === 5) return 'Sat'
+  }
+
   return (
-    <Link className='bookingCardContainer' to={`/spots/${booking.Spot.id}/${booking.Spot.ownerId}`}>
+    <Link className='bookingCardContainer' to={`/confirmed/${booking.Spot.id}/${booking.id}`}>
       <div className='bookingCardContainer'>
         <img className='bookingImage' src={booking.Spot.previewImage} />
         <div className='bookingDescription'>
+
           <div className='bookingDescriptionLeft'>
             <div className='bookingLocation'>{booking.Spot.city}, {booking.Spot.state}</div>
             <div className='bookingCountry'>{booking.Spot.country}</div>
-            {/* <div className='bookingPrice'>
-              <div className='bookingCardPriceAmount'>{`$${booking.Spot.price}`}</div>
-              <div className='bookingCardNight'>night</div>
-            </div> */}
-            <div>{booking.startDate}</div>
-            <div>{booking.endDate}</div>
-          </div>
-          <div className='bookingDescriptionRight'>
-            {/* <div className='bookingStar'>
-              <i className="fa-solid fa-star"></i>
+
+            <div className='bookingCardDates'>
+              <div>{`${weekday(startInt)}, ${booking?.startDate} - ${weekday(endInt)}, ${booking?.endDate}`}</div>
+              {/* <div>{`${weekday(endInt)}, ${booking?.endDate}`}</div> */}
             </div>
-            <div className='bookingRating'>{booking.Spot.avgRating}</div> */}
+
+            {/* <div className='bookingCardCancel'>Cancel</div> */}
+
           </div>
+
+          <div className='bookingDescriptionRight'>
+
+          </div>
+
         </div>
       </div>
     </Link>
